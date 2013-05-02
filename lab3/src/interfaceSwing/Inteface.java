5d180f44692fd3b4668136ac3220713c1b210b10
Thalysson Davyd Vieira <thalysson.vieira@ccc.ufcg.edu.br>
2013-05-02 14:28:02 -0300

    iniciando lab3

---
+++ lab3/src/interfaceSwing/Inteface.java
@@ -0,0 +1,5 @@
+package interfaceSwing;
+
+public class Inteface {
+
+}
---
+++ lab3/src/testes/Testes.java
@@ -0,0 +1,23 @@
+package testes;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+public class Testes {
+
+	@Before
+	public void configure() {
+	
+	}
+	
+	@Test
+	public void testaAbrirArquivo(){
+		
+	}
+	
+	@Test
+	public void testaContagemPalavras(){
+		
+	}
+}
